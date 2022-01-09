28724634f510e8dcd362d861cf50de5c79fc8db2
Mikołaj Wewiór <mikifda@gmail.com>
2022-01-10 00:13:32 +0100

    new functions included to main()

--- game.py
+++ game.py
@@ -11,33 +11,26 @@ def insert_guide():
 def main():
     no_solution = 'There is no solutioin. Puzzle cannot be solved.'
     guide = insert_guide()
-    table0 = guide.set_table()
-    table_over = guide.solve_if_N(guide.solve_if_ONE(table0))
-    if guide.is_table_correct(table_over) is False:
+    table = guide.set_table()
+    guide.solve_if_N(guide.solve_if_ONE(table))
+    if guide.is_table_correct(table) is False:
         return no_solution
-    # kolejność następnych funkcji
-    # 1) zero_into_list()
-    # 2) reduction_from_guide()
-    # 3) limit_potential_solutions
-    #       +++
-    #           trzeba jeszcze rozważyć to, że wszystkie mogą być listą
-    #           i jesli w rzedzie/kolumnie jakis element jest tylko
-    #           w jednej liscie to on musi wyjsc z listy
-    #           i on zostaje umieszczony w tabeli
-    #       +++
-    #
-    #  ### ==> na ten moment mielibysmy zrobiony etap E4 a nawet chyba E5
+    guide.zero_into_list(table)
+    guide.reduce_from_guide_overall(table)
+    guide.limit_potential_solutions(table)
 
-    # 1) DONE - mamy wprowadzone warunki konieczne - sprawdzenie dla N i dla 1
-    #    (jeśli one nie są okej to na pewno jest źle)
-    # 2) DONE - zera zamieniamy na listy zawierające od 1 do N
-    # 3) DONE - usuwamy najwyższe wartości stojące przy podpowiedzi
-    # 4) NOT YET - usuwamy z list te wartości, które już są w rzędzie(kolumnie)
-    # 5) porównuje listy - jeśli w jakiejś liście jest taki element,
-    #    który nie pojawiwa się w innych to go wpisujemy w pozycje na planszy
-    # 6) kroki 4 i 5 powtarzamy dopóki można
-    # 7) gdy już nie da się skróić bardziej, to trzeba uwzględnić te opcje,
-    #    które są zgodne z podpowiedzią
+    # na tym etapie mamy utworzoną tablicę, na której naniesione
+    # są już częściowo odpoweidzi.
+    # Tam, gdzie nie da się jednoznczanie wpisać wartości
+    # jest lista możliwych opcji.
+    #
+    # Teraz potrzeba funkcji, które:
+    #   - podstawią jakąś opcję, która jest najbardziej prawdopodobna,
+    #   - limiter() z tą opcją
+    #   - sprawdzająca, czy wartość jest poprawna i
+    #     czy zgadza się z wartością z guide'a ( counter() )
+    #
+    #   - jeśli jescze się nie rozwiązało powtarzamy
 
 
 if __name__ == '__main__':
